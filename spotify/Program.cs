a3a94820813a23a21f9819fae5cbb3d6e54df24d
Renzo <renzovanijzendoorn2@gmail.com>
2022-04-06 14:43:21 +0200

    added class files
    added class files

--- spotify/Program.cs
+++ spotify/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using album;
+using friendlist;
+using music;
+using person;
+using playlist;
+using playlistlibrary;
+namespace Spotify{ 
+    class spotify
+    {
+
+    // Main Method
+        static public void Main(String[] args)
+        {
+
+            Console.WriteLine("Main Method");
+        }
+    }
+}
