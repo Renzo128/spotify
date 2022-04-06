808a4b8d3e031bb8fbfad42bb2daf8136f6d5cba
Renzo <renzovanijzendoorn2@gmail.com>
2022-04-06 16:23:18 +0200

    update friendlist
    update friendlist

--- spotify/Friendlist.cs
+++ spotify/Friendlist.cs
@@ -4,9 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using person;
+using playlist;
 namespace friendlist
 {
     public class Friendlist
     {
+        private List<Person> friends = new List<Person>();
+
+        public Friendlist()
+        {
+
+        }
+        public void addFriend(Person person)
+        {
+            friends.Add(person);    
+        }
+        public void removeFriend(Person person)
+        {
+            friends.Remove(person);
+        }
+
+        public void searchFriends(Person person)
+        {
+
+        }
+
+        public void viewPlaylistOfFriends(Person person)
+        {
+
+        }
+        public void copyPlaylist(Playlist playlist)
+        {
+
+        }
+
     }
+
+
 }
